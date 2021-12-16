d0865c996367e81b5a16c69e36a453551bea0c96
Naoki Sugimura <coco.915@icloud.com>
2021-12-17 08:49:07 +0900

    x,y,θを計算するクラス作成

--- src/main.py
+++ src/main.py
@@ -1,33 +1,96 @@
-import cv2
 import numpy as np
+from time import sleep
+
 
 
 class Movement_matrix:
+
+    # 低レイヤークラス
+
     def __init__(self):
-        self.R = 0.29
-        self.x = np.array([0])
-        self.y = np.array([0])
-        self.theta = np.array([0])
-        self.t = 0.01
+        self.R = 0.29 # 車輪半径[m]
+        self.d = 0.4  # 車輪と車体中心までの距離[m]
+        self.pulse_num = 40 # パルス数
+        self.x = np.array([0.1]) # x座標のlist
+        self.y = np.array([0.1]) # y座標のlist
+        self.theta = np.array([0]) # thetaのlist
+        self.t = 0.01 # 時間 [s]
 
+    def calculate_theta_l(self):
+        theta_element = self.theta[-1] + 2 * np.pi * self.R * self.pulse_num / 1600
+        self.theta = np.append(self.theta, theta_element)
+        # print(self.theta[-1])
+        return self.theta
 
-    def cal_v_and_omega(self,f_right,f_left):
-        self.v = self.R*(f_right+f_left)/(3200*2)
-        self.omega = self.R*(f_right-f_left)/(3200*2)
-        return self.v, self.omega
+    def calculate_theta_r(self):
+        theta_element = self.theta[-1] - 2 * np.pi * self.R * self.pulse_num / 1600
+        self.theta = np.append(self.theta, theta_element)
+        # print(self.theta[-1])
+        return self.theta
 
-    def calculate(self):
-        x_element = self.v*np.cos(self.theta[-1])*self.t + self.x[-1]
-        y_element = self.v*np.sin(self.theta[-1])*self.t + self.y[-1]
-        theta_element = self.omega*self.t + self.theta[-1]
+    def calculate_theta_s(self):
+        theta_element = self.theta[-1]
+        self.theta = np.append(self.theta, theta_element)
+        # print(self.theta[-1])
+        return self.theta
+
+    def calculate_x_and_y(self):
+        x_element = np.cos(self.theta[-2]) * self.x[-1] - np.sin(self.theta[-2]) * self.y[-1]
+        y_element = np.sin(self.theta[-2]) * self.x[-1] + np.cos(self.theta[-2]) * self.y[-1]
         self.x = np.append(self.x,x_element)
         self.y = np.append(self.y,y_element)
-        self.theta =np.append(self.theta,theta_element)
-        print(self.x)
-        return self.x, self.y, self.theta
+        print(self.x[-1])
+        print(self.y[-1])
+        return self.x[-1], self.y[-1], self.theta[-1]
+
+
+
+class Movement_Handler(Movement_matrix):
+    # 左折　直進　右折に対応したクラス
+    def turn_left(self):
+        self.calculate_theta_l()
+        self.calculate_x_and_y()
+
+    def go_straight(self):
+        self.calculate_theta_s()
+        self.calculate_x_and_y()
+
+    def turn_right(self):
+        self.calculate_theta_r()
+        self.calculate_x_and_y()
+
+    def judge_movement(self,serial_string:str):
+        if serial_string == 'l':
+            self.turn_left()
+            print('l')
+
+        elif serial_string == 's':
+            self.go_straight()
+            print('s')
+
+
+        elif serial_string == 'r':
+            self.turn_right()
+            print('r')
+
+
+        elif serial_string == 'e':
+            pass
+            print('e')
+
+
+        else:
+            raise Exception()
+    
 
 if __name__ == '__main__':
-    a = Movement_matrix()
+    import serial
+    # ser = serial.Serial('COM4', 9600, timeout = 0.1)
+    # serial_str = ser.read()
+    a = Movement_Handler()
     while True:
-        a.cal_v_and_omega(100,150)
-        a.calculate()
+        serial_str = input()
+        s = input()
+        a.judge_movement(serial_string = serial_str)
+        sleep(0.1)
+    # ser.close()
